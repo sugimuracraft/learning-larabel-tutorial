1e5f58dcbcce4e3f4fcd1d9b43bfce0ba63fe67a
Kohei Sugimura <sugimurakohei@sugimuracraft.com>
2023-11-14 08:17:34 +0900

    add filter for Customer.

---
+++ example-app/app/Services/V1/CustomerQuery.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services\V1;
+
+use Illuminate\Http\Request;
+
+class CustomerQuery {
+    protected $safeParams = [
+        'name' => ['eq'],
+        'type' => ['eq'],
+        'email' => ['eq'],
+        'address' => ['eq'],
+        'city' => ['eq'],
+        'state' => ['eq'],
+        'postalCode' => ['eq', 'gt', 'lt'],
+    ];
+
+    /**
+     * The map to convert JSON field name to DB column name.
+     */
+    protected $columnMap = [
+        'postalCode' => 'postal_code',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>=',
+    ];
+
+    public function transform(Request $request) : array {
+        $eloQuery = [];
+
+        foreach ($this->safeParams as $param => $operators) {
+            $query = $request->query($param);
+
+            if (!isset($query)) {
+                continue;
+            }
+
+            $column = $this->columnMap[$param] ?? $param;
+
+            foreach ($operators as $operator) {
+                if (!isset($query[$operator])) {
+                    continue;
+                }
+                $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+            }
+        }
+
+        return $eloQuery;
+    }
+}
