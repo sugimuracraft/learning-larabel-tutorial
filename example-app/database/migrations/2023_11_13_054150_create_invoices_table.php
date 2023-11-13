260a096b7c25b491ad6598715f4cdd9448461f87
Kohei Sugimura <sugimurakohei@sugimuracraft.com>
2023-11-14 08:17:34 +0900

    implements migrations.

--- example-app/database/migrations/2023_11_13_054150_create_invoices_table.php
+++ example-app/database/migrations/2023_11_13_054150_create_invoices_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->integer('customer_id');
+            $table->integer('amount');
+            $table->string('status');  // "B"illed, "P"aid, "V"oid
+            $table->dateTime('billed_at');
+            $table->dateTime('paid_at')->nullable();
             $table->timestamps();
         });
     }
