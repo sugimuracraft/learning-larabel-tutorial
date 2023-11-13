e5b87d3f3de8f5ec46e1e98ce5526431ad46eb01
Kohei Sugimura <sugimurakohei@sugimuracraft.com>
2023-11-14 08:17:34 +0900

    apply pagination to Customer.

--- example-app/app/Http/Controllers/Api/V1/CustomerController.php
+++ example-app/app/Http/Controllers/Api/V1/CustomerController.php
@@ -16,7 +16,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return new CustomerCollection(Customer::all());
+        return new CustomerCollection(Customer::paginate());
     }
 
     /**
