78445665c529df7e0280841dd6000e3d68e75cfe
Kohei Sugimura <sugimurakohei@sugimuracraft.com>
2023-11-14 09:42:43 +0900

    add customer upadteion api.

--- example-app/app/Http/Controllers/Api/V1/CustomerController.php
+++ example-app/app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Filters\V1\CustomersFilter;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
@@ -74,7 +74,7 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update(($request->all()));
     }
 
     /**
