ff4f14eae908416ef9cbfd976b535225bb13893d
Kohei Sugimura <sugimurakohei@sugimuracraft.com>
2023-11-14 14:12:40 +0900

    add invoice bulk creation api.

--- example-app/app/Http/Controllers/Api/V1/InvoiceController.php
+++ example-app/app/Http/Controllers/Api/V1/InvoiceController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Filters\V1\InvoicesFilter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Resources\V1\InvoiceCollection;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
@@ -44,6 +46,18 @@ class InvoiceController extends Controller
     public function store(StoreInvoiceRequest $request)
     {
         //
+    }
+
+    /**
+     * Store some newly created resources in storage.
+     */
+    public function bulkStore(BulkStoreInvoiceRequest $request)
+    {
+        $bulk = collect($request->all())->map(function($arr, $key) {
+            return Arr::except($arr, ['customerId', 'billedAt', 'paidAt']);
+        });
+
+        Invoice::insert($bulk->toArray());
     }
 
     /**
---
+++ example-app/app/Http/Requests/V1/BulkStoreInvoiceRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class BulkStoreInvoiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            '*.customerId' => ['required', 'integer'],
+            '*.amount' => ['required', 'numeric'],
+            '*.status' => ['required', Rule::in(['B', 'P', 'V', 'b', 'p', 'v'])],
+            '*.billedAt' => ['required', 'date_format:Y-m-d H:i:s'],
+            '*.paidAt' => ['date_format:Y-m-d H:i:s', 'nullable'],
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $data = [];
+
+        foreach ($this->toArray() as $obj) {
+            $obj['customer_id'] = $obj['customerId'] ?? null;
+            $obj['billed_at'] = $obj['billedAt'] ?? null;
+            $obj['paid_at'] = $obj['paidAt'] ?? null;
+
+            $data[] = $obj;
+        }
+
+        $this->merge($data);
+    }
+}
